5c2db69012130e08c6246ce3f4936aa21039e9d6
Riley Farro <rileyfarro@gmail.com>
2025-01-19 18:07:28 +0800

    Create file manager. Initial functions are get duration of audio file and convert string uri to file.

---
+++ app/src/main/java/com/rpfcoding/echo_journal/core/file/data/AndroidFileManager.kt
@@ -0,0 +1,32 @@
+package com.rpfcoding.echo_journal.core.file.data
+
+import android.content.Context
+import android.media.MediaMetadataRetriever
+import android.net.Uri
+import androidx.core.net.toFile
+import androidx.core.net.toUri
+import com.rpfcoding.echo_journal.core.file.domain.FileManager
+import java.io.File
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
+
+class AndroidFileManager(
+    private val context: Context
+): FileManager {
+
+    override fun getDurationOfAudioFile(file: File): Long {
+        val mmr = MediaMetadataRetriever().apply {
+            setDataSource(context, file.toUri())
+        }
+        val duration = mmr
+            .extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
+            ?.toIntOrNull()
+            ?.toDuration(DurationUnit.MILLISECONDS)
+            ?.toLong(DurationUnit.SECONDS)
+        return duration ?: -1L
+    }
+
+    override fun getFileFromUri(value: String): File {
+        return Uri.parse(value).toFile()
+    }
+}
---
+++ app/src/main/java/com/rpfcoding/echo_journal/core/file/domain/FileManager.kt
@@ -0,0 +1,14 @@
+package com.rpfcoding.echo_journal.core.file.domain
+
+import java.io.File
+
+interface FileManager {
+
+    /**
+     * @param file The audio file.
+     * @return duration in seconds or -1 if the file is invalid or not found.
+     */
+    fun getDurationOfAudioFile(file: File): Long
+
+    fun getFileFromUri(value: String): File
+}
